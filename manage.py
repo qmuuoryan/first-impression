63b2365315083a002c8eb6098959aa0e9428731d
ryan <you@example.com>
2019-02-13 01:12:16 +0300

    addd changes

--- manage.py
+++ manage.py
@@ -1,12 +1,12 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,roles
-
-
-
+from app.models import User,Role
+from  flask_migrate import Migrate, MigrateCommand
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Roles=roles )
+    return dict(app = app,db = db,User = User, Role=Role )
 if __name__ == '__main__':
     manager.run()
+
+    
