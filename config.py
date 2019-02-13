88852df5ff576a153a13031183a9586629d205e7
ryan <you@example.com>
2019-02-13 08:38:35 +0300

    add config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ryan:ryanmuuo91@localhost/first_impessions'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    SQLALCHEMY_TRACK_MODIFICATIONS = Fa
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
