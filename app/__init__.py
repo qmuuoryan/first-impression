bf5eb90a7b282e4e8fad50e856ccabf41cf73897
ryan <you@example.com>
2019-02-14 20:26:36 +0300

    modified

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 
 
@@ -9,3 +11,19 @@ def create_app(config_name):
     #...
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+photos = UploadSet('photos',IMAGES)
+def create_app(config_name):
+    app = Flask(__name__)
+
+     # configure UploadSet
+    configure_uploads(app,photos)
+class Config:
+
+    mail = Mail()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    #........
+    mail.init_app(app)
+#........
