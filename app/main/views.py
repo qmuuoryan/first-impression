f64548ad368f458782c925d0baae77ee1745026f
ryan <you@example.com>
2019-02-14 20:27:43 +0300

    modified

---
+++ app/main/views.py
@@ -0,0 +1,46 @@
+from flask_login import login_required
+from .forms import ReviewForm,UpdateProfile
+from .. import db,photos
+
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    pitch_count = Pitch.count_pitches(uname)
+
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html',user = user, pitches = pitch_count)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
