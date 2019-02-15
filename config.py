ebbc34541708c85c48da742331636dac83d08127
ryan <you@example.com>
2019-02-15 07:36:16 +0300

    add chanegs

--- config.py
+++ config.py
@@ -1,28 +1,29 @@
 import os
 
 class Config:
-    SECRET_KEY = 'ryanmuuo91'
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ryan:ryanmuuo91@localhost/impressions'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ryanmuuo:ryanmuuo91@localhost/pitch'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABSE_URL")
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ryan:ryanmuuo91@localhost/impressions_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ryanmuuo:ryanmuuo91@localhost/pitch_test'
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ryan:ryanmuuo91@localhost/first_impessions'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ryanmuo91:ryanmuuo91@localhost/pitch'
     DEBUG = True
-
 
 config_options = {
 'development':DevConfig,
 'production':ProdConfig,
 'test':TestConfig
+}
